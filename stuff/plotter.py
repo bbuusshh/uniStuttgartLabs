de408712d5d724285b8dbe9d284afd7a2bde1f30
bbuusshh <bushmakinvls@gmail.com>
2019-04-15 16:44:00 +0200

    Various stuff

--- stuff/plotter.py
+++ stuff/plotter.py
@@ -11,7 +11,7 @@ def simplePlot(x, y, errorX, errorY, xLabel, yLabel, plotName):
     ax.set_ylabel(yLabel, fontsize=20)
     ax.set_xlabel(xLabel, fontsize=20)
     ax.tick_params(direction='in', length=5, width=1, colors='k')
-    
+
     fig.tight_layout()
     fig.savefig(plotName + '.png', dpi = 300)
     return fig, ax
